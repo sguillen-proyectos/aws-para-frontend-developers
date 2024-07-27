fee20378a6de7698d92fe42db80c66b508ea0425
Sergio Guillen <serguimant@gmail.com>
2024-07-27 10:31:40 -0400

    Add change to main landing page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <p id="zero-state">
-      AWS Para Frontend Developers
+      AWS Para Frontend Developers que no fueron a la entrada universitaria
       <br />
       Siguenos en {" "}
       <a href="https://www.meetup.com/aws-user-group-la-paz/">
